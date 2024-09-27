4998f72e78991335535130d3ff3dd20700e7ff28
Vishal Kaleria <w3vish@gmail.com>
2024-09-27 08:26:36 -0700

    Update pages index: Add GridContent component import and export

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,9 +1,11 @@
 import BasePokemonCard from "./BasePokemonCard";
 import PokemonCard from "./PokemonCard";
 import PaginationClient from "./PaginationClient";
+import GridContent from "./PokemonsGrid";
 
 export {
     PokemonCard,
     BasePokemonCard,
     PaginationClient,
+    GridContent,
 }
