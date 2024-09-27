d211b186bf0d6d03b6de02e04ca25dc9272416c3
Vishal Kaleria <w3vish@gmail.com>
2024-09-27 08:25:08 -0700

    Refactor SpritesGallary: Use GridContent component and adjust padding

--- src/components/sprites/SpritesGallary.tsx
+++ src/components/sprites/SpritesGallary.tsx
@@ -2,10 +2,11 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { getSpriteImageURL, generateArtistSlug } from "@/lib/utils";
-import { placeHolders } from "@/lib/utils/constants";
+import { gridClass, placeHolders } from "@/lib/utils/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { GridContent } from "../pages";
 
 type Images = Array<{
   sprite_id: string;
@@ -20,9 +21,9 @@ interface SpritesGallaryProps {
 const SpritesGallary = ({ images }: SpritesGallaryProps) => {
 
   return (
-    <Card className="my-4">
+    <Card className="my-4 py-4">
       <CardHeader className="text-2xl text-center p-1">Sprites Gallery ({images.length})</CardHeader>
-      <CardContent className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
+      <GridContent>
         {images.map((image, index) => {
           const spriteURL =
             image.sprite_type === "autogen"
@@ -65,7 +66,7 @@ const SpritesGallary = ({ images }: SpritesGallaryProps) => {
             </div>
           );
         })}
-      </CardContent>
+      </GridContent>
     </Card>
   );
 };
