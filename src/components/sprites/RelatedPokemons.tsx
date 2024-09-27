1ade16e6d7f446d738a2c1e773549becd6431ecd
Vishal Kaleria <w3vish@gmail.com>
2024-09-27 08:25:07 -0700

    Update RelatedPokemons: Use gridClass for consistent styling

--- src/components/sprites/RelatedPokemons.tsx
+++ src/components/sprites/RelatedPokemons.tsx
@@ -1,6 +1,7 @@
 import { loadSprite } from "@/lib/utils";
 import { PokemonCard } from "../pages";
 import { Card, CardContent, CardHeader } from "../ui/card";
+import { gridClass } from "@/lib/utils/constants";
 
 type RelatedPokemonArgs = {
     id: string,
@@ -16,7 +17,7 @@ const RelatedPokemons = async ({ id, spriteType }: RelatedPokemonArgs) => {
     return (
         <Card className="my-4">
             <CardHeader className="text-2xl text-center p-1">Related Pokemons </CardHeader>
-            <CardContent className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <CardContent className={gridClass}>
                 {
                     data.results.map(pokemon => <PokemonCard pokemon={pokemon} key={pokemon.id} />)
                 }
