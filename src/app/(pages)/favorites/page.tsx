ae28ffeda7e269749d840da019cff0d351384da3
Vishal Kaleria <w3vish@gmail.com>
2024-09-28 08:31:16 -0700

    Refactor favorites page for server-side rendering and add metadata

--- src/app/(pages)/favorites/page.tsx
+++ src/app/(pages)/favorites/page.tsx
@@ -1,60 +1,19 @@
-"use client"; // Required to interact with localStorage
-import { useState, useEffect } from "react";
-import { GridContent, PokemonCard } from "@/components/pages";
-import { Separator } from "@/components/ui/separator"; 
-import Link from "next/link";
+import React from 'react'
+import FavoritesClient from './FavoritesClient'
+import { Metadata } from 'next'
 
-// Define the structure for Pokémon data based on what was stored
-interface PokemonData {
-    id: string;
-    name: string;
-    primary_type?: string;
-    secondary_type?: string;
-    base_pokemons: { [key: string]: string };
-    total_sprites: number;
-    images: {
-      sprite_id: string;
-      sprite_type: string;
-      artists: string[];
-    }[];
+
+export const metadata: Metadata = {
+  title: 'Favorites',
+  description: 'View and manage your favorite Pokémon sprites on this page.',
+  robots: {
+    index: false,
+    follow: false
   }
-  
+}
 
-const storageKey = "LikedSprites";
+function Page() {
+  return <FavoritesClient />
+}
 
-const FavoritesPage = () => {
-  const [favoritePokemons, setFavoritePokemons] = useState<PokemonData[]>([]);
-
- 
-  useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem(storageKey) || "[]");
-    setFavoritePokemons(storedFavorites);
-  }, []);
-
-  return (
-    <div className="container">
-      <h1 className="text-2xl font-bold m-4">My Favorite Sprites</h1>
-      
-      <Separator className="mb-4" />
-
-      {/* Show a message if no favorites are found */}
-      {favoritePokemons.length === 0 ? (
-        <div className="text-muted-foreground text-lg">
-          <p>You don&lsquo;t have any favorite Pokémon sprites yet.</p>
-          <Link href="/" className="underline">
-            Explore Pokémon sprites to add some favorites!
-          </Link>
-        </div>
-      ) : (
-        <GridContent>
-          {/* Render PokemonCard for each favorite Pokémon */}
-          {favoritePokemons.map((pokemon: PokemonData) => (
-            <PokemonCard key={pokemon.id} pokemon={pokemon} />
-          ))}
-        </GridContent>
-      )}
-    </div>
-  );
-};
-
-export default FavoritesPage;
+export default Page
