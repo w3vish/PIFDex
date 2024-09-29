82f38841d67b1bd9a4beb96375efbcf88a3a2313
Vishal Kaleria <w3vish@gmail.com>
2024-09-29 06:32:15 -0700

    Fix Errors Hope it Works :)

--- src/app/api/fusion/[id]/route.ts
+++ src/app/api/fusion/[id]/route.ts
@@ -1,35 +1,30 @@
-import { parseJsonResults } from "@/lib/utils";
+import { getMainSpriteId, parseJsonResults } from "@/lib/utils";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 import { NextRequest } from "next/server";
 
-export const runtime = 'edge';
+export const runtime = 'edge'
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    // Input validation
     if (!params?.id) {
         return Response.json({ error: 'ID parameter is required' }, { status: 400 });
     }
-
     const db = getRequestContext().env.DB;
     const ids = [params.id, params.id.split('.').reverse().join('.')];
 
-    // SQL query for fetching the sprite data
+    let query;
+    let bindValues: string[] = [];
+
     const placeholders = ids.map(() => '?').join(',');
-    const query = `SELECT * FROM sprites WHERE id IN (${placeholders})`;
+    query = `SELECT * FROM sprites WHERE id IN (${placeholders})`;
+    bindValues = ids;
+
 
     try {
-        const stmt = await db.prepare(query).bind(...ids).all();
+        const stmt = await db.prepare(query).bind(...bindValues).all();
         const data = parseJsonResults(stmt);
-
-        // If no data is found, return 404
-        if (data.length === 0) {
-            return Response.json({ error: 'Sprite not found' }, { status: 404 });
-        }
-
-        // Return the found data
-        return new Response(data, { status: 200 });
+        return Response.json(data);
     } catch (error: any) {
-        // Catching and handling any database query errors
         return Response.json({ error: 'Database query failed', details: error.message }, { status: 500 });
     }
+
 }
