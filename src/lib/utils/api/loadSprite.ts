a9ae5dce1cc760f7be8370d11bd76b5d1ed6ead4
Vishal Kaleria <w3vish@gmail.com>
2024-10-09 05:13:58 -0700

    Define a cached version of the loadSprite function

--- src/lib/utils/api/loadSprite.ts
+++ src/lib/utils/api/loadSprite.ts
@@ -1,11 +1,15 @@
 import { SpriteResponse } from "@/lib/types";
 import { apiURL } from "../constants";
+import { cache } from "react";
 
-  // Define the loadSprite function with proper typing
-  const loadSprite = async (id: string): Promise<SpriteResponse> => {
-    const res = await fetch(`${apiURL}/sprites/${id}`);
-    const sprite: SpriteResponse = await res.json();
-    return sprite;
-  };
-  
-  export {loadSprite};
+// Define a cached version of the loadSprite function
+const loadSprite = cache(async (id: string): Promise<SpriteResponse> => {
+  const res = await fetch(`${apiURL}/sprites/${id}`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch sprite with id: ${id}`);
+  }
+  const sprite: SpriteResponse = await res.json();
+  return sprite;
+});
+
+export { loadSprite };
