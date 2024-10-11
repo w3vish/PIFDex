a3645bf8e7fecaf688bc3e658d71052cad7237f1
Vishal Kaleria <w3vish@gmail.com>
2024-10-10 23:48:44 -0700

    add skelaton

--- src/components/fusion/FusionResult.tsx
+++ src/components/fusion/FusionResult.tsx
@@ -9,6 +9,7 @@ import { FusionStats } from './FusionStats';
 import { FusionAbilities } from './FusionAbilities';
 import { generateStats } from '@/lib/utils/fusion';
 import { FusionWeaknesses } from './FusionWeaknesses';
+import { Skeleton } from '../ui/skeleton';
 
 interface FusionResultProps {
   fusionStatus: 'idle' | 'loading' | 'success' | 'error';
@@ -42,24 +43,24 @@ export function FusionResult({ fusionStatus, headData, bodyData }: FusionResultP
   if (fusionStatus === 'success' && headData && bodyData) {
     return (
       <article className='space-y-6 sm:p-4 max-w-4xl mx-auto'>
-       
-          <div className="grid grid-cols-2 gap-1 sm:gap-4">
-            <div>
-              <FusionImage pokemon={headData} />
-            </div>
-            <div>
-              <FusionImage pokemon={bodyData} />
-            </div>
+
+        <div className="grid grid-cols-2 gap-1 sm:gap-4">
+          <div>
+            <FusionImage pokemon={headData} />
           </div>
-          <div className="grid grid-cols-2 gap-2 md:gap-8 md:px-4">
-            <FusionStats stats={generateStats(headData)} comparisonStats={generateStats(bodyData)} />
-            <FusionStats stats={generateStats(bodyData)} comparisonStats={generateStats(headData)} />
+          <div>
+            <FusionImage pokemon={bodyData} />
           </div>
-          <div className="grid grid-cols-2 gap-2 md:gap-8 md:px-4">
-            <FusionAbilities ability={{ normal: headData.abilities, hidden: headData.hidden_abilities }} />
-            <FusionAbilities ability={{ normal: bodyData.abilities, hidden: bodyData.hidden_abilities }} />
-          </div>
-       
+        </div>
+        <div className="grid grid-cols-2 gap-2 md:gap-8 md:px-4">
+          <FusionStats stats={generateStats(headData)} comparisonStats={generateStats(bodyData)} />
+          <FusionStats stats={generateStats(bodyData)} comparisonStats={generateStats(headData)} />
+        </div>
+        <div className="grid grid-cols-2 gap-2 md:gap-8 md:px-4">
+          <FusionAbilities ability={{ normal: headData.abilities, hidden: headData.hidden_abilities }} />
+          <FusionAbilities ability={{ normal: bodyData.abilities, hidden: bodyData.hidden_abilities }} />
+        </div>
+
         <div>
           <FusionWeaknesses types={{
             headTypes: [headData.primary_type, headData.secondary_type],
@@ -76,8 +77,59 @@ export function FusionResult({ fusionStatus, headData, bodyData }: FusionResultP
 
 function FusionLoading() {
   return (
-    <div className="text-center p-4">
-      <p>Loading fusion data...</p>
-    </div>
+    <article className='space-y-6 sm:p-4 max-w-4xl mx-auto animate-pulse'>
+      <div className="grid grid-cols-2 gap-4 sm:gap-8">
+        {[0, 1].map((index) => (
+          <div key={index} className='flex flex-col items-center max-w-xs mx-auto w-full'>
+            <div className="relative w-full aspect-square">
+              <Skeleton className="absolute inset-0 rounded-lg" />
+              <div className="absolute bottom-2 left-2 flex gap-1">
+                <Skeleton className="h-6 w-16 rounded-full" />
+                <Skeleton className="h-6 w-16 rounded-full" />
+              </div>
+            </div>
+            {/* <Skeleton className="h-6 w-3/4 mt-2" /> */}
+            <Skeleton className="h-6 w-1/2 mt-1" />
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:gap-8">
+        {[0, 1].map((index) => (
+          <Card key={index}>
+            <CardContent className="p-4">
+              <Skeleton className="h-5 w-1/2 mb-3" />
+              {[0, 1, 2, 3, 4, 5].map((statIndex) => (
+                <div key={statIndex} className="flex items-center mb-2">
+                  <Skeleton className="h-4 w-20 mr-2" />
+                  <Skeleton className="h-4 flex-grow" />
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:gap-8">
+        {[0, 1].map((index) => (
+          <Card key={index}>
+            <CardContent className="p-4">
+              <Skeleton className="h-5 w-1/3 mb-3" />
+              <Skeleton className="h-4 w-2/3 mb-2" />
+              <Skeleton className="h-4 w-1/2" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Card>
+        <CardContent className="p-4">
+          <Skeleton className="h-5 w-1/4 mb-3" />
+          <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+            {[0, 1, 2, 3].map((index) => (
+              <Skeleton className="h-16 mb-2" />
+             
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </article>
   );
 }
