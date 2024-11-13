d1e69c452b229520c001f709f8be44224d22217a
Vishal Kaleria <w3vish@gmail.com>
2024-11-13 12:34:25 +0530

    add google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
           <Toaster />
         </ThemeProvider>
         <GoogleAnalytics gaId="G-X91KPL7L5C" />
-        <script defer={true} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5359451330187418"
+        <script defer={true} src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6617838414961747"
           crossOrigin="anonymous"></script>
       </body>
     </html>
